5c2fff76f99a79808c31fac3623494437d79eace
Alexander Chernyavskiy <git@ch3rn1k.me>
2021-02-01 10:44:31 +0300

    first commit

---
+++ webpack.common.js
@@ -0,0 +1,65 @@
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+  entry: {
+    app: './src/index.js',
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      title: 'Hello World',
+      template: path.resolve(__dirname, './src/template.html'),
+      filename: 'index.html',
+      meta: {
+        viewport: 'width=device-width, initial-scale=1',
+        charset: 'UTF-8',
+        author: 'ch3rn1k'
+      }
+    }),
+    new CopyPlugin({
+      patterns: [
+        { from: path.resolve(__dirname, './src/assets/images'), to: 'img' }
+      ]
+    })
+  ],
+  output: {
+    filename: '[name].[contenthash].bundle.js',
+    path: path.resolve(__dirname, './build')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      },
+      {
+        test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
+        type: 'asset/resource'
+      },
+      {
+        test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'fonts/'
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(scss|css)$/,
+        use: ['style-loader', 'css-loader', 'postcss-loader', 'sass-loader']
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource'
+      }
+    ]
+  }
+}
