ccdc10bbb01f6006843728b99741b61b22e9cb2b
Alexander Chernyavskiy <git@ch3rn1k.me>
2021-06-01 11:01:15 +0300

    moved lang into files

---
+++ src/language/en.js
@@ -0,0 +1,26 @@
+const ms = {
+  welcome: {
+    typicalIT: 'Typical IT',
+    aLotOfCode: 'Some code',
+    bitDesign: 'Bit design',
+    someHW: 'Pinch HW',
+    helloDude: 'Hello, %username%!',
+    myName: 'My name is ch3rn1k and I am Fullstack JS.',
+    miscText: [
+      'Yeah, NodeJS, MERN and all of this stuff!',
+      'Just touched C# и C++, but survived and returned to the WEB. :D'
+    ],
+    whatIAmDoing: [
+      'Creating user-friendly interfaces in complex applications - UX/UI.',
+      'And of course, custom PC\'s, with water coolings, yum.'
+    ]
+  },
+  portfolio: {
+    title: 'Portfolio',
+    thatsMe: 'As a matter of fact, here I am:'
+  },
+  messageMePlease: 'Write me, do not be shy!',
+  key: 'en'
+};
+
+export default ms;
---
+++ src/language/ru.js
@@ -0,0 +1,26 @@
+const ms = {
+  welcome: {
+    typicalIT: 'Типичный IT',
+    aLotOfCode: 'Много кода',
+    bitDesign: 'Немного дизайна',
+    someHW: 'Щепотка железа',
+    helloDude: 'Привет, %username%!',
+    myName: 'Меня зовут ch3rn1k и я Fullstack JS.',
+    miscText: [
+      'Да да, NodeJS, MERN и все в этом духе!',
+      'Слегка потрогал C# и C++, но остался жив и вернулся в WEB. :D'
+    ],
+    whatIAmDoing: [
+      'Создаю удобные интерфейсы в сложных приложениях - UX/UI.',
+      'Ну и конечно же крафт "железа", с кастомными водянками, ням.'
+    ]
+  },
+  portfolio: {
+    title: 'Портфолио',
+    thatsMe: 'Собственно говоря, вот и я:'
+  },
+  messageMePlease: 'Пиши, не стесняйся!',
+  key: 'ru'
+};
+
+export default ms;
